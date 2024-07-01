b78b46f26f083b300c4dbe7d7b345fee4d1ca286
Isabelle <isabelle965rizzo@gmail.com>
2024-07-01 10:34:51 -0300

    adding note

--- src/app/user/tasks/page.js
+++ src/app/user/tasks/page.js
@@ -16,6 +16,23 @@ export default function AllTasksPage() {
 	// const taskList = [];
 	const [ taskList, setTaskList ] = useState([]);
 
+	// TESTAR DE NOVO ESSE USECALLBACK
+	// const loadTasks = useCallback(async () => {
+	// 	console.log('[loadTasks]');
+	// 	const tasksRes = await listAllTasksService();
+
+	// 	if (!tasksRes.success) {
+	// 		throw new Error(tasksRes.message);
+	// 	}
+
+	// 	if (taskList.length > 0) {
+	// 		console.log('[loadTasks] taskList: ', taskList);
+	// 		taskList.push(...tasksRes.result);
+	// 	}
+
+	// 	console.log('[loadTasks] taskList: ', taskList);
+	// }, [taskList]);
+
 	useEffect(() => {
 		async function loadTasks() {
 			const res = await listAllTasksService();
