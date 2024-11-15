d679e8ca2b016af7bd3dd49632804a80e402de92
Isabelle Rizzo <62210690+irizzo@users.noreply.github.com>
2024-11-15 20:11:19 -0300

    ajustando datas de tarefas e eventos (card) (#101)

--- src/app/user/(sidebar)/events/page.js
+++ src/app/user/(sidebar)/events/page.js
@@ -2,6 +2,7 @@
 
 import * as locale from '@/resources/locale'
 import { routesMap } from '@/resources/routesMap'
+import { groupEventsByDate } from '@/utils/groupData.utils'
 
 import { useEffect, useState, useContext } from 'react'
 import { UserCategoriesContext, UserEventsContext } from '@/hooks'
@@ -12,14 +13,12 @@ import { EventCard } from '@/components/Card'
 import { GeneralInfo } from '@/components/Messages'
 
 export default function EventsPage() {
-	const [ categories, setCategories ] = useState({})
 	const { userCategories, setUserCategories } = useContext(UserCategoriesContext)
 	const { userEvents, setUserEvents } = useContext(UserEventsContext)
 
 	const [ isLoading, setIsLoading ] = useState(false)
-
-	const pastDueEvents = []
-	const dueSoonEvents = []
+	const [ categories, setCategories ] = useState({})
+	const [ sortedEvents, setSortedEvents ] = useState({})
 
 	useEffect(() => {
 		if (userCategories.categoriesList === null || userEvents.eventsList === null) {
@@ -27,13 +26,17 @@ export default function EventsPage() {
 
 			setTimeout(() => {
 				setIsLoading(false)
-			}, 2000)
+			}, 1000)
 		}
 
 		let aux = {}
 		userCategories.categoriesList.forEach((category) => {
 			aux[category.code] = category.title
 		})
+
+		const sorted = groupEventsByDate(userEvents.eventsList)
+		setSortedEvents({ ...sorted })
+
 		setCategories(aux)
 
 	}, [ userCategories.categoriesList, userEvents.eventsList ])
@@ -43,6 +46,7 @@ export default function EventsPage() {
 	return (
 		<>
 			<Board title={locale.pagesTitles.events.base} path={routesMap.events.new}>
+				{console.log('sortedEvents: ', sortedEvents)}
 				{
 					userEvents.eventsList !== null && userEvents.eventsList.length > 0 ?
 						userEvents.eventsList.map((event) => <EventCard key={event.id} eventInfo={event} categoryTitle={categories[event.categoryCode]} />)
