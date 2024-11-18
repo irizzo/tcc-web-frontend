e9cb41091c037791c0581948e30b6c706ea0b180
Isabelle <isabelle965rizzo@gmail.com>
2024-11-18 13:40:53 -0300

    managing host for production

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,6 @@
 'use server'
 import { NextRequest, NextResponse } from 'next/server'
+import { headers } from 'next/headers.js'
 
 import { routesMap } from '@/resources/routesMap'
 import { verifyUserAuthService } from '@/services/userAccessServices'
@@ -9,19 +10,13 @@ console.debug('[middleware] CURRENT_ENV: ', process.env.CURRENT_ENV)
 console.debug('[middleware] APP_PROD_BASEURL: ', process.env.APP_PROD_BASEURL)
 console.debug('[middleware] APP_DEV_BASEURL: ', process.env.APP_DEV_BASEURL)
 console.debug('[middleware] API_PROD_BASEURL: ', process.env.API_PROD_BASEURL)
-console.debug('[middleware] window.location.href: ', window.location.href)
+console.debug('[authMiddleware] 1st APP_BASEURL: ', APP_BASEURL)
+
 
 if (!APP_BASEURL) {
 	console.debug('no baseurl')
 	APP_BASEURL = 'https://lifesync-ir.vercel.app/'
 }
-if (APP_BASEURL !== window.location.href) {
-	const splitURL = window.location.href.slice(8).split('/')
-	console.debug('[middleware] `https://${splitURL[0]}`: ', `https://${splitURL[0]}`)
-	APP_BASEURL = `https://${splitURL[0]}`
-}
-console.debug('[middleware] APP_BASEURL: ', APP_BASEURL)
-
 
 /**
  * authMiddleware
@@ -31,10 +26,22 @@ console.debug('[middleware] APP_BASEURL: ', APP_BASEURL)
 export default async function authMiddleware(req) {
 	console.debug('[authMiddleware]')
 
+	const headersList = headers()
+	const currentHost = headersList.get('host')
+	const currentAPPHost = APP_BASEURL.split('/')
+	console.debug('[authMiddleware] currentAPPHost: ', currentAPPHost)
+
+	if (currentAPPHost[2] !== currentHost) {
+		console.debug('[authMiddleware] diferente')
+		APP_BASEURL = `${currentAPPHost[0]}://${currentHost}`
+		console.debug('[authMiddleware] new APP_BASEURL: ', APP_BASEURL)
+	}
+
 	let isUserLogged = false
 	const tokenCookie = req.cookies.get('token')
 
 	const { pathname } = req.nextUrl
+
 	const userBasePath = '/user'
 
 	if (pathname === '/') return NextResponse.next()
